b8faa5da28effc4b15b149a404dd492c377e8224
DQing <15829258737@163.com>
2018-07-11 19:37:30 +0800

    init project
    Signed-off-by: DQing <15829258737@163.com>

---
+++ src/main/java/com/twu/BibliotecaApp/BibliotecaAppApplication.java
@@ -0,0 +1,12 @@
+package com.twu.BibliotecaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/twu/BibliotecaApp/BibliotecaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twu.BibliotecaApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BibliotecaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
